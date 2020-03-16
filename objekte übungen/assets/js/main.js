4aea3791c543cbf7a3b23bb3d4756a0eb5a3fa67
Marcin <mgabrys422@gmail.com>
2020-03-16 14:07:55 +0100

    First

---
+++ objekte übungen/assets/js/main.js
@@ -0,0 +1,34 @@
+let europaeischeUnion = {
+
+    Belgien: "BE",
+    Griechenland: "EL",
+    Litauen: "LT",
+    Portugel: "PT",
+    Bulgarien: "BG",
+    Spanien: "ES",
+    Luxemburg: "LU",
+    Rumaenien: "RO",
+    Tschechien: "CZ",
+    Frankreich: "FR",
+    Ungarn: "HU",
+    Slowenien: "SI",
+    Dänemark: "DK",
+    Kroatien: "HR",
+    Malta: "MT",
+    Slowenien: "SK",
+    Deutschland: "DE",
+    Italien: "IT",
+    Niederlande: "NL",
+    Finnland: "FI",
+    Estland: "EE",
+    Zypern: "CY",
+    Österreich: "AT",
+    Schweden: "SE",
+    Irland: "IE",
+    Lettland: "LV",
+    Polen: "PL",
+    VereinigtesKoenigreich: "UK"
+
+}
+
+
