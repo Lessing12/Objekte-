3bd240d650605889d3141e7df1fcc337d9576613
Marcin <mgabrys422@gmail.com>
2020-03-18 13:50:27 +0100

    Spiel

---
+++ cluedo/assets/js/main.js
@@ -0,0 +1,35 @@
+//Erstellen wir also für jeden Kartentyp ein Array, 
+// das die Objekte enthält, die jede Karte repräsentieren.
+//  Diese Arrays sollten als suspectsArray, weaponsArray,
+//   roomsArray benannt werden.
+
+
+const suspectsArra =
+{
+    firstName: 'Jacob',
+    lastName: 'Green',
+    occupation: 'Entrepreneur'
+
+}
+    // {
+    //     firstName: "Anna",
+    //     lastName: "Black",
+    //     occupation: "Designer";
+    // }
+
+    ;
+console.log(suspectsArray);
+
+
+
+// const weaponsArray = [{ name: "pistole", "gewicht 300gr"},]{
+
+// }
+
+// var fruits, text;
+// fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// text = "<ul>";
+// fruits.forEach(myFunction);
+// text += "</ul>";
+// document.getElementById("demo").innerHTML = text;
